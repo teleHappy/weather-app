75ce18fb1e1ab9d3e90a8b555d0025bceeb190a8
Rick Dillon <rdillon.enservio@gmail.com>
2024-02-07 09:56:19 -0500

    adding typescript

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'; // Import the 'React' module
+
+import './App.css';
+import WeatherComponent from './Weather';
+
+function App() {
+  return (
+    <>
+      <WeatherComponent />
+    </>
+  );
+}
+
+export default App;
