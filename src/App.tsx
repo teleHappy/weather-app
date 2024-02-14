112d7ba7872c74f9dac50e5a141271b625a7221f
Rick Dillon <rdillon.enservio@gmail.com>
2024-02-14 16:12:40 -0500

    Add react-router-dom dependency and update App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react'; // Import the 'React' module
-
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import './App.css';
 import WeatherComponent from './WeatherComponent';
 
 function App() {
   return (
     <>      
-      <WeatherComponent />
+    <Router>
+      <Routes>
+        <Route path="/" element={<WeatherComponent />}/>
+      </Routes>
+    </Router>
     </>
   );
 }
