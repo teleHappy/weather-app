bf3eb300f8031054981fb8f090ba23561347ec34
Rick Dillon <rdillon.enservio@gmail.com>
2024-03-06 14:11:06 -0500

    Remove unused import statement in LocationHeader.tsx

--- src/components/location/LocationHeader.tsx
+++ src/components/location/LocationHeader.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useRef, useState } from 'react';
+import React, { useContext, useState } from 'react';
 import { CurrentLocationContext } from '../../context/CurrentLocationContext';
 import LocationDialog from './LocationDialog';
 
